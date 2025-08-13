fa436d3e54a3f8c841b8ff072bbc5bcb8713403f
Kshitij Tripathi <58387989+Kshitij3003@users.noreply.github.com>
2025-08-13 21:19:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   type        = string
   description = "The region to deploy resources in"
-  default     = ""us-central1""
+  default     = "us-central1"
 }
 
 variable "zone" {
   type        = string
   description = "The zone to deploy resources in"
-  default     = ""us-central1-a""
+  default     = "us-central1-a"
 }
