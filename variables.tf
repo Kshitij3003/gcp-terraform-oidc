fa93932f5b2e25113882f19ccde4084d64e0e12d
Kshitij Tripathi <58387989+Kshitij3003@users.noreply.github.com>
2025-08-13 21:27:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project"
+  default = "amplified-brook-468711-k5"
+}
+
 variable "region" {
   type        = string
   description = "The region to deploy resources in"
