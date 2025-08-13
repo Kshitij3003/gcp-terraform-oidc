e20ba7936281822713f9f10cca58cb0e975b23ed
Kshitij Tripathi <58387989+Kshitij3003@users.noreply.github.com>
2025-08-13 20:52:27 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type        = string
+  description = "The region to deploy resources in"
+  default     = ""REGION""
+}
+
+variable "zone" {
+  type        = string
+  description = "The zone to deploy resources in"
+  default     = ""ZONE""
+}
